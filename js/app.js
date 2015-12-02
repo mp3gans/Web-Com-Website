2a591e4a0cd2512f0bdbcbb056ab24749349d150
mp3gans <mp3gans@yahoo.com>
2015-12-02 09:48:09 -0500

    more progress

--- js/app.js
+++ js/app.js
@@ -5,83 +5,34 @@
     app = angular.module('store', []);
     gems = [
         {
-            name: 'Dope Diamond',
-            price: 82.95,
+            name: 'Old Fashion Chicken Soup',
             description: "8 cups Chicken Stock or fat-free, lower-sodium chicken broth\n2 (4-ounce) skinless, bone-in chicken thighs\n1 (12-ounce) skinless, bone-in chicken breast half\n2 cups diagonally sliced carrot\n2 cups diagonally sliced celery\n1 cup chopped onion\n6 ounces uncooked medium egg noodles\n1/2 teaspoon kosher salt\n1/2 teaspoon black pepper\nCelery leaves (optional)",
-            specification: "Step 1: Combine the chicken stock, chicken thighs and breast half in a Dutch oven over medium-high heat\nStep 2: Bring to a boil. Reduce heat. Simmer for 20 minutes\nStep 3: Remove chicken from pan. Let stand for 10 minutes\nStep 4: Remove chicken from bones. Shred meat into bite-sized pieces. Discard bones\nStep 5: Add carrot, celery, and onion to pan. Cover and simmer for 10 minutes\nStep 6: Add noodles and simmer 6 minutes\nStep 7: Add chicken, salt, and black pepper. Cook for 2 minutes or until noodles are done\nStep 8: Garnish with celery leaves, if desired",
-            review: "Maecenas quis metus izzle fo shizzle my nizzle yo gangsta. Break yo neck, yall dope viverra fo. Curabitizzle sollicitudin boom shackalack quizzle purus.",
-            canPurchase: true,
-            soldOut: false,
+            specification: "Step 1: Combine the chicken stock, chicken thighs and breast half in a Dutch oven over medium-high heat.\nStep 2: Bring to a boil. Reduce heat. Simmer for 20 minutes.\nStep 3: Remove chicken from pan. Let stand for 10 minutes.\nStep 4: Remove chicken from bones. Shred meat into bite-sized pieces. Discard bones.\nStep 5: Add carrot, celery, and onion to pan. Cover and simmer for 10 minutes.\nStep 6: Add noodles and simmer 6 minutes.\nStep 7: Add chicken, salt, and black pepper. Cook for 2 minutes or until noodles are done.\nStep 8: Garnish with celery leaves, if desired.",
             images: [
                 {
-                    full: 'img/diamond.png',
-                    thumb: 'img/diamond-thumb.png'
+                    full: 'img/Chicken-Soup.jpg'
                 }
             ],
             reviews: [
                 {
                     stars: 5,
-                    body: "I love this gem",
+                    body: "I love this soup",
                     author: "joe@thomas.com"
                 },
                 {
                     stars: 3,
-                    body: "I bought this for my wife",
+                    body: "I made this for my wife",
                     author: "hubby@loveydovey.com"
                 }
             ]
         },
         {
-            name: 'Rare Ruby',
-            price: 95.95,
-            description: "Maecenas quis metus izzle fo shizzle my nizzle yo gangsta. Break yo neck, yall dope viverra fo. Curabitizzle sollicitudin boom shackalack quizzle purus.",
-            specification: "Lorizzle ipsizzle dolor sit dang, we gonna chung that's the shizzle shut the shizzle up. Nullam tellivizzle bling bling, aliquet volutpizzle, suscipit break it down, gravida.",
-            review: "Phasellizzle fo shizzle my nizzle crunk. Curabitur non velizzle the bizzle pede crackalackin facilisizzle. Bow wow wow rizzle nulla, iaculizzle fo shizzle, break.",
-            canPurchase: true,
-            soldOut: false,
+            name: 'Cold Chicken and Rice Salad',
+            description: "2 (6-ounce) skinless, boneless chicken breast halves\n3/4 teaspoon salt, divided\n3/4 teaspoon black pepper, divided\nCooking spray\n3/4 cup uncooked long-grain rice\n3 tablespoons fresh lemon juice\n2 tablespoons olive oil\n1/3 cup dried cranberries\n1/4 cup diced celery\n1/4 cup thinly sliced green onions\n1/4 cup diced red bell pepper\n1/4 cup chopped pimiento-stuffed olives\n12 green leaf lettuce leaves, torn\n1/4 cup thinly sliced green onion tops\n4 lemon wedges",
+            specification: "Step 1: Preheat oven to 400°.\nStep 2: Sprinkle chicken with 1/4 teaspoon salt and 1/2 teaspoon pepper.\nStep 3: Heat a medium ovenproof skillet over medium-high heat. Coat pan with cooking spray.\nStep 4: Add chicken to pan; cook 3 minutes. Turn chicken over. Place pan in oven; bake at 400° for 8 minutes or until done.\nStep 5: Remove chicken from pan, and let stand for 5 minutes. Shred chicken, and chill for 30 minutes.\nStep 6: Cook rice according to package directions, omitting salt and fat.\nStep 7: Coat a jelly-roll pan with cooking spray. Spread rice in an even layer in pan. Chill for 30 minutes.\nStep 8: Combine remaining 1/2 teaspoon salt, remaining 1/4 teaspoon pepper, juice, and oil in a large bowl, stirring with a whisk.\nStep 9: Add chicken, rice, cranberries, and next 5 ingredients (through lettuce) to juice mixture in bowl, and toss gently.\nStep 10: Divide the rice mixture evenly among 4 plates, and sprinkle with green onion tops. Serve with lemon wedges.",
             images: [
                 {
-                    full: 'img/ruby.png',
-                    thumb: 'img/ruby-thumb.png'
-                }
-            ],
-            reviews: [
-                {
-                    stars: 4,
-                    body: "This Ruby is really hard to find",
-                    author: "jane@doe.com"
-                },
-                {
-                    stars: 2,
-                    body: "Mine came cracked",
-                    author: "james@jungle.com"
-                }
-            ]
-        }
-    ];
-    
-    food = [
-        {
-            name: 'Old Fashion Chicken Soup',
-            calories: 82.95,
-            ingredients: [
-            ],
-            preparation: [
-                {
-                    "Step 1": '',
-                    "Step 2": '',
-                    "Step 3": '',
-                    "Step 4": '',
-                    "Step 5": '',
-                    "Step 6": '',
-                    "Step 7": '',
-                    "Step 8": ''
-                }
-            ],
-            images: [
-                {
-                    full: 'img/diamond.png',
-                    thumb: 'img/diamond-thumb.png'
+                    full: 'img/Chicken-Rice-Salad.jpg'
                 }
             ],
             reviews: [
